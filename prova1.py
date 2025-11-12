caecf33c16bd394f61a395c8f915c129e7aa03fb
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-11-12 13:42:14 +0100

    ex1.c

--- prova1.py
+++ prova1.py
@@ -3,6 +3,13 @@ b = float(input("Escriure el primer segon nombre: "))
 f= float(input("Escriure el primer tercer nombre: "))
 c = a + b + f
 print("El resultat de la suma {} + {} + {} és {}" .format(a, b, f, c))
-c = a * b
+if c>20:
+    print("La suma és major que 20, que és {}".format(c))
+else:
+    print("La suma és menor que 20, que és {}".format(c))
+c = a * b * f
 print("El resultat de la multiplicació {} * {} * {} és {}".format (a, b, f, c))
-
+if c>100:
+    print("La multiplicació és major que 100, que és {}".format(c))
+else:
+    print("La multiplicació és menor que 100, que és {}".format(c))
