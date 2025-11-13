12836787a4c6c31facccc7a7f7886c097c57bc7f
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-11-13 11:53:53 +0100

    ex12.c

--- ex12.py
+++ ex12.py
@@ -1,7 +1,12 @@
-edat = int(input("Escriu la seva edat: "))
-if edat > 18:
-    print("Es major d'edat")
-elif edat<18:
-    print ("Es menor d'edat")
-else:
-    print("Enhorabona, aquest any has fet 18 anys")
+def majoredat(edat):
+    if edat > 18:
+        print("Es major d'edat")
+    elif edat<18:
+        print ("Es menor d'edat")
+    else:
+        print("Enhorabona, aquest any has fet 18 anys")
+        
+edat = int(input("Escriure la seva edat: "))
+majoredat(edat)
+edat = int(input("Escriure la seva edat: "))
+majoredat(edat)
