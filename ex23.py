c7e9b1d183af50bb0d7e35237071a8c3dc1b0e85
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-12-12 09:31:12 +0100

    ex23.py

---
+++ ex23.py
@@ -0,0 +1,10 @@
+def crear_repetits(n, car):
+    """
+    Retorna el caràcter 'car' repetit 'n' vegades.
+    """
+    return car * n
+
+# Exemples de prova
+print(crear_repetits(5, "a"))  # aaaaa
+print(crear_repetits(3, "x"))  # xxx
+print(crear_repetits(0, "b"))  # cadena buida
