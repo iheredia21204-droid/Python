b643e906a910fb9872ba1f5e05e035f7503bae42
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-12-12 17:36:54 +0100

    ex57.py

---
+++ ex57.py
@@ -0,0 +1,7 @@
+# Primer augmentem la longitud de les línies
+for i in range(1, 4):  # 1, 2, 3
+    print('*' * i)
+
+# Després disminuïm la longitud de les línies
+for i in range(2, 0, -1):  # 2, 1
+    print('*' * i)
