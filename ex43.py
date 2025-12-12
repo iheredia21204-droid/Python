06ae2bdfedfecb96dd59c93b7b8eb647e5d15f2b
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-12-12 17:05:56 +0100

    ex43.py

---
+++ ex43.py
@@ -0,0 +1,4 @@
+for i in range(5):  # Repetim 5 vegades
+    for j in range(1, 16):  # Números de l'1 al 15
+        print(j, end=" ")  # Imprimim en la mateixa línia
+    print()  # Saltem de línia després de cada sèrie
