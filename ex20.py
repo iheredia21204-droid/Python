1523f10ea9b66a6752880eb438173e8a7a27ec05
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-12-12 09:22:25 +0100

    ex20.py

---
+++ ex20.py
@@ -0,0 +1,10 @@
+def invertir(cadena):
+    """
+    Devuelve la cadena invertida.
+    """
+    return cadena[::-1]
+
+# Ejemplos de prueba
+print(invertir("Soc del Ramis"))   # simaR led coS
+print(invertir("Hola"))            # aloH
+print(invertir("Python"))          # nohtyP
