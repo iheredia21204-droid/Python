8e7987fcc1bf2f180d3f6db021456b19b7b1379b
iheredia21204-droid <iheredia21204@iesjoanramis.org>
2025-12-12 16:18:12 +0100

    ex30.py

---
+++ ex30.py
@@ -0,0 +1,7 @@
+def binari_a_enter(binari):
+    return int(binari, 2)
+
+# Proves
+print(binari_a_enter("1010"))    # 10
+print(binari_a_enter("1111"))    # 15
+print(binari_a_enter("100000"))  # 32
